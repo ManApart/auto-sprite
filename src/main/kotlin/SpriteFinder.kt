83f351bac5074b91e89b75dac789200ffee01aff
ManApart <raustinkucera@gmail.com>
2020-11-07 11:01:30 -0500

    find multiple sprites

--- src/main/kotlin/SpriteFinder.kt
+++ src/main/kotlin/SpriteFinder.kt
@@ -1,8 +1,18 @@
 fun findSprites(image: SpriteSheet, backgroundColor: Int = 0): List<Sprite> {
-    val closed = mutableListOf<Pixel>()
-    val seed = image.pixels.values.firstOrNull { it.color != backgroundColor }
-    val open = listOfNotNull(seed).toMutableList()
+    val seeds = image.pixels.values.filter { it.color != backgroundColor }.toMutableList()
+    val sprites = mutableListOf<Sprite>()
+    while (seeds.isNotEmpty()) {
+        val sprite = findSprite(seeds.first(), image, backgroundColor)
+        sprites.add(sprite)
+        seeds.removeAll(sprite.pixels)
+    }
 
+    return sprites
+}
+
+private fun findSprite(seed: Pixel, image: SpriteSheet, backgroundColor: Int): Sprite {
+    val open = listOf(seed).toMutableList()
+    val closed = mutableListOf<Pixel>()
     val pixels = mutableListOf<Pixel>()
 
     while (open.isNotEmpty()) {
@@ -14,6 +24,5 @@ fun findSprites(image: SpriteSheet, backgroundColor: Int = 0): List<Sprite> {
         val neighbors = image.getNeighbors(next).filter { it.color != backgroundColor }.filter { !closed.contains(it) }
         open.addAll(neighbors)
     }
-
-    return listOf(Sprite(pixels))
+    return Sprite(pixels)
 }
--- src/test/kotlin/SpriteFinderTest.kt
+++ src/test/kotlin/SpriteFinderTest.kt
@@ -18,4 +18,34 @@ class SpriteFinderTest {
         assertEquals(1, sprites.size)
         assertEquals(5, sprites.first().pixels.size)
     }
+
+    @Test
+    fun findTwoSprites() {
+        val image = buildSpriteSheet(
+            listOf(
+                listOf(1, 1, 0, 0, 0),
+                listOf(1, 0, 1, 0, 0),
+                listOf(0, 1, 1, 1, 0),
+                listOf(0, 0, 1, 0, 0)
+            )
+        )
+        val sprites = findSprites(image, 0)
+
+        assertEquals(2, sprites.size)
+        assertEquals(3, sprites.first().pixels.size)
+        assertEquals(5, sprites.last().pixels.size)
+    }
+
+    @Test
+    fun findNoSprites() {
+        val image = buildSpriteSheet(
+            listOf(
+                listOf(0, 0, 0, 0, 0),
+                listOf(0, 0, 0, 0, 0)
+            )
+        )
+        val sprites = findSprites(image, 0)
+
+        assertEquals(0, sprites.size)
+    }
 }
