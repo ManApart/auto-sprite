e5c17942146d431074eeccdeb3af5b9e65377a0f
ManApart <raustinkucera@gmail.com>
2020-11-07 20:48:11 -0500

    attempt to preserve sprite order

--- src/main/kotlin/SpriteSheetBuilder.kt
+++ src/main/kotlin/SpriteSheetBuilder.kt
@@ -3,18 +3,20 @@ fun createSpriteSheet(grid: Grid, sprites: List<Sprite>): SpriteSheet {
     var originY = 0
     var column = 0
 
-    val newSprites = sprites.map { sprite ->
-        if (column >= grid.columns) {
-            column = 0
-            originX = -grid.width
-            originY += grid.height
+    val newSprites = sprites
+        .sortedWith(compareBy({ it.pixels.first().y }, { it.pixels.first().x }))
+        .map { sprite ->
+            if (column >= grid.columns) {
+                column = 0
+                originX = -grid.width
+                originY += grid.height
+            }
+            column += 1
+            originX += grid.width
+
+
+            sprite.moveTo(originX, originY)
         }
-        column += 1
-        originX += grid.width
-
-
-        sprite.moveTo(originX, originY)
-    }
 
     return SpriteSheet(newSprites)
 }
